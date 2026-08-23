a9c6684373d705ade34a72b48bf528d3ec8e4259
Johnson Lien <johnsonlien95@gmail.com>
2026-08-23 11:46:27 -0700

    fix zoomstrategy class to not use method calling using 'self.'

--- src/ptz_camera_track/camera/zoom.py
+++ src/ptz_camera_track/camera/zoom.py
@@ -3,21 +3,24 @@ import cv2
 class ZoomStrategy:
     def __init__(self):
         self.zoom_strategies = {
-            'affine': self.zoom_affine,
-            #'crop': self.zoom_crop
+            'affine': ZoomStrategy.zoom_affine,
+            #'crop': ZoomStrategy.zoom_crop
         }
 
-    def zoom(self, frame, zoom_scale=1.5, center=None, strategy="affine"):
+    def zoom(self, frame, zoom_scale : float = 1.5, center=None, strategy="affine"):
         if strategy in self.zoom_strategies:
             kwargs = {'zoom_scale': zoom_scale, 'center': center}
             return self.zoom_strategies[strategy](frame, **kwargs)
 
         print(f"Warning: Zoom strategy '{strategy}' was not found.")
-        return 
+        return frame 
     
     @staticmethod
-    def zoom_affine(frame, zoom_scale=1.5, center=None):
+    def zoom_affine(frame, zoom_scale : float = 1.5, center : tuple[int, int] = None):
         height, width = frame.shape[:2]
+        
+        if zoom_scale <= 1.0:
+            return frame
 
         if center is None:
             cx, cy = width / 2.0, height / 2.0
@@ -27,7 +30,25 @@ class ZoomStrategy:
         matrix = cv2.getRotationMatrix2D((cx, cy), 0, zoom_scale)
 
         return cv2.warpAffine(frame, matrix, (width, height), flags=cv2.INTER_LINEAR)
-
-    def zoom_crop(frame, zoom_scale=1.5, center=None):
-        print("zoom cropping")
-        pass
+    
+#    @staticmethod
+#    def zoom_crop(frame, zoom_scale : float = 1.5, center : tuple[int, int] = None):
+#        height, width = frame.shape[:2]
+#
+#        if zoom_scale <= 1.0:
+#            return frame
+#        
+#
+#        scaled_width, scaled_height = int(width / zoom_scale), int(height / zoom_scale)
+#        
+#        if center is None:
+#            center = (0, 0)
+#
+#        if center is None:
+#            y1 = int((height - scaled_height) / 2)
+#            y2 = y1 + scaled_height
+#            x1 = int(( width - scaled_width) / 2)
+#            x2 = x1 + scaled_width
+#            cropped = frame[y1:y2, x1:x2]
+#
+#
