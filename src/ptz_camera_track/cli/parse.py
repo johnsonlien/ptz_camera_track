aa43371610b429b57fa43e85bf59efec1094e598
Johnson Lien <johnsonlien95@gmail.com>
2026-08-23 17:16:53 -0700

    change default pins for servos

--- src/ptz_camera_track/cli/parse.py
+++ src/ptz_camera_track/cli/parse.py
@@ -7,7 +7,7 @@ def get_cli_parser():
     )
     # Tracking 
     tracking_group = parser.add_argument_group("Tracking Settings")
-    tracking_group.add_argument("-m", "--model", choices=["yolo11n.pt, yolo26n.pt"], default="yolo26n.pt")
+    tracking_group.add_argument("-m", "--model", type=str, default="yolo11n_fish.pt", help="Path to model. If not found, will try to download from Ultralytics")
     tracking_group.add_argument("-c", "--confidence", default=0.5)
     tracking_group.add_argument("-tc", "--track-config", choices=["bytetrack", "botsort"], default="botsort")
     tracking_group.add_argument("-t", "--target", type=str, help="Select a category to track", default="person")
@@ -22,13 +22,13 @@ def get_cli_parser():
 
     # Movement-related
     movement_group = parser.add_argument_group("Movement Settings", "Change GPIO Servo settings")
-    movement_group.add_argument("-pp", "--pan-pin", help="Set the GPIO panning pin", type=int, default=23)
+    movement_group.add_argument("-pp", "--pan-pin", help="Set the GPIO panning pin", type=int, default=13)
     movement_group.add_argument("--pan-min-angle", help="Set the panning minimum angle in degrees", default=-60.0, type=float)
     movement_group.add_argument("--pan-max_angle", help="Set the panning maximum angle in degrees", default=60.0, type=float)
     movement_group.add_argument("--pan-min-pulse", help="Set the panning minimum pulse width", default=0.6/1000, type=float)
     movement_group.add_argument("--pan-max-pulse", help="Set the panning maximum pulse width", default= 2.3/1000, type=float)
     
-    movement_group.add_argument("-tp", "--tilt-pin", help="Set the GPIO tilting pin", type=int, default=24)
+    movement_group.add_argument("-tp", "--tilt-pin", help="Set the GPIO tilting pin", type=int, default=12)
     movement_group.add_argument("--tilt-min-angle", help="Set the tilting minimum angle in degrees", default=-60.0, type=float)
     movement_group.add_argument("--tilt-max-angle", help="Set the tilting maximum angle in degrees", default=60.0, type=float)
     movement_group.add_argument("--tilt-min-pulse", help="Set the tilting minimum pulse width", default=0.6/1000, type=float)
