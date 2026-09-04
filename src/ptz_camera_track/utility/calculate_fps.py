1c4deb7639cd4470d0eeebf1d349a3545ac03fc2
Johnson Lien <johnsonlien95@gmail.com>
2026-09-03 20:47:22 -0700

    fix fps caluclation

--- src/ptz_camera_track/utility/calculate_fps.py
+++ src/ptz_camera_track/utility/calculate_fps.py
@@ -3,26 +3,26 @@ import cv2
   
 class FPSCalculator:
     def __init__(self, alpha : float = 0.1):
-        self.alpha = 0.1
-        self.prev_time = 0
-        self.smoothed_fps = 0
+        self.alpha = alpha
+        self.prev_time = None
+        self.smoothed_time_diff = 0
 
     def calculate_fps(self, frame) -> None:
         cur_time = time.time()
-        time_diff = cur_time - self.prev_time
 
-        if time_diff > 0:
-            raw_fps = 1 / time_diff
-        else:
-            raw_fps = 0
+        if self.prev_time is not None:
+            time_diff = cur_time - self.prev_time
+            self.smoothed_time_diff = (
+                (self.alpha * time_diff) + ((1.0 - self.alpha) * self.smoothed_time_diff)
+            )
 
         self.prev_time = cur_time
 
-        self.smoothed_fps = (self.alpha * raw_fps) + ( (1.0 - self.alpha) * self.smoothed_fps)
+        smoothed_fps = 1 / self.smoothed_time_diff if self.smoothed_time_diff > 0 else 0
 
         cv2.putText(
             frame,
-            f"FPS: {self.smoothed_fps:.1f}",
+            f"FPS: {smoothed_fps:.1f}",
             (10, 40), # Top left corner
             cv2.FONT_HERSHEY_SIMPLEX,
             1,
